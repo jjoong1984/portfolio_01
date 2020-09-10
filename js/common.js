db56d5633b1d16d687f44edd040a1449739b899b
jjoong1984 <68522549+jjoong1984@users.noreply.github.com>
2020-09-10 17:39:06 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,27 @@
+(function($){ 
+
+    $('#jjContainer_Box').load(main.html)
+
+    $('.topmenu_box > login > a').on('click',function(){ 
+        var url = $(this).attr('href')
+        $('#jjContainer').remove
+        $('#jjContainer_Box').load(url)
+        return false
+    })
+
+    $('.h1Nav > h1 > a').on('click',function(){ 
+        var url = $(this).attr('href')
+        $('#jjContainer').remove
+        $('#jjContainer_Box').load(url)
+        return false
+    })
+
+    $('.logo > a').on('click',function(){ 
+        var url = $(this).attr('href')
+        $('#jjContainer').remove
+        $('#jjContainer_Box').load(url)
+        return false
+    })
+
+
+})(jQuery)
