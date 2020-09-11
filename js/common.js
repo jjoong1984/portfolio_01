b3427f8cc1e1158e73c4c41f81aeb310225ea237
jjoong1984 <joong1984@gmail.com>
2020-09-11 18:16:02 +0900

    0911

--- js/common.js
+++ js/common.js
@@ -9,19 +9,20 @@
         return false
     })
 
-    $('.h1Nav > h1 > a').on('click',function(){ 
-        var url = $(this).attr('href')
-        $('#jjContainer').remove
-        $('#jjContainer_Box').load(url)
-        return false
-    })
+    // $('.h1Nav > h1 > a').on('click',function(){ 
+    //     var url = $(this).attr('href')
+    //     $('#jjContainer').remove
+    //     $('#jjContainer_Box').load(url)
+    //     return false
+    // })
 
-    $('.logo > a').on('click',function(){ 
-        var url = $(this).attr('href')
-        $('#jjContainer').remove
-        $('#jjContainer_Box').load(url)
-        return false
-    })
+    // $('.logo > a').on('click',function(){ 
+    //     var url = $(this).attr('href')
+    //     $('#jjContainer').remove
+    //     $('#jjContainer_Box').load(url)
+    //     return false
+    // })
+
 
 
 })(jQuery)
