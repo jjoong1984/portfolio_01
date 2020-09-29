72df4939bd5c87e32f2c960dfb67b05a84c4a5d8
jjoong1984 <joong1984@gmail.com>
2020-09-29 14:29:24 +0900

    resize 작업중 0929

--- js/common.js
+++ js/common.js
@@ -42,28 +42,89 @@
         return false
     })
 
+    init();
+    init2();
+    var flag = true
+    function init(){ 
+        var ww = $(window).width()
+        if ( ww > 767 && flag ) { 
+            $('.nav').show()
+            $('.open_nav').hide()
+            $('.close_nav').hide()
+            $('.depth2').hide()
+            flag = false
+
+        } else if ( ww <= 767 && !flag ) { 
+            $('.open_nav').show()
+            $('.nav').hide()
+            $('.deco_box').hide()
+            $('.depth2').hide()
+            flag = true
+        }
+    }
+    function init2(){ 
+        var ww = $(window).width()
+        if ( ww > 767 ) { 
+            $('.depth1 > li').hover(
+                function(){ 
+                    $('.depth2').stop().slideDown(300)
+                    $('.deco_box').stop().slideDown(300)
+                },
+                function(){ 
+                    $('.depth2').stop().slideUp(300)
+                    $('.deco_box').stop().slideUp(300)
+                } 
+            )
+        } else if (  ww <= 767 ) {
+            $('.depth1 > li > a').on('click', function(){ 
+                $('.depth2').stop().slideToggle(300)
+
+            }) 
+          
+        }
+    }
+    $(window).on('resize', function(){ 
+        init()
+        init2()
+    })
+
+
     $('.h1Nav .open_nav').on('click', function(){ 
         $(this).stop().fadeOut(300)
         $(this).nextAll('.close_nav').stop().fadeIn(300)
-        $(this).next().slideDown(300)
+        $(this).next().stop().slideDown(300)
     })
     $('.h1Nav .close_nav').on('click', function(){ 
         $(this).stop().fadeOut(300)
         $(this).prevAll('.open_nav').stop().fadeIn(300)
-        $(this).prev().slideUp(300)
+        $(this).prev().stop().slideUp(300)
     })
 
-
-    $('.depth1 > li').hover(
-        function(){ 
-            $('.depth2').stop().slideDown(300)
-            $('.deco_box').stop().slideDown(300)
-        },
-        function(){ 
-            $('.depth2').stop().slideUp(200)
-            $('.deco_box').stop().slideUp(200)
-        }
-    )
+    
+    // $('.depth1 > li').hover(
+    //     function(){ 
+    //         var ww = $(window).width()
+    //         if(ww > 767) {
+    //             $('.depth2').stop().slideDown(300)
+    //             $('.deco_box').stop().slideDown(300)
+    //         }
+    //         else { 
+    //             $('.depth2').hide()
+    //             $('.deco_box').hide()
+    //         }    
+    //     },
+    //     function(){ 
+    //         var ww = $(window).width()
+    //         if(ww > 767) {
+    //          $('.depth2').stop().slideUp(200)
+    //          $('.deco_box').stop().slideUp(200)
+    //         } 
+    //         else { 
+    //             $('.depth2').hide()
+    //             $('.deco_box').hide()
+    //         }
+    //     } 
+    // )
 
     $('.goTop').on('click', function(){ 
         $('html, body').stop().animate({
